04efbb99a3c8234709e11ab8befea2f1997a248b
mikettigit <acepost@protonmail.ch>
2017-03-07 10:31:54 +0300

    mobile version

---
+++ _MvcApplication16_m.natpot/MvcApplication16_m.natpot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcApplication16_m.natpot.MvcApplication" Language="C#" %>
---
+++ _MvcApplication16_m.natpot/MvcApplication16_m.natpot/js/custom.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+
+    $(".SendMessage").click(function () {
+        $('.quform-outer').toggleClass("expand");
+        return false;
+    })
+
+})
