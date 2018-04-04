fb25a2d4d209a8feb6bfc3c181f766876dfe3734
mikettigit <acepost@protonmail.ch>
2018-04-04 16:14:09 +0300

    part2

---
+++ Content/themes/ictec/scripts/ictec.js
@@ -0,0 +1,75 @@
+﻿function goToByScroll(id) {
+	$("html,body").animate({ scrollTop: $("#" + id).offset().top-20 }, 1000);
+}
+$(window).scroll(function () { 
+	currentScrollTop = $(this).scrollTop();
+	if (currentScrollTop > 600) {
+			$(".moveTop").fadeIn(500);
+	}
+	else {
+			$(".moveTop").fadeOut(500);
+	}
+});
+
+$(document).ready(function () {
+
+	//Gallery <br />
+	$(".gallery").find("br").remove();
+
+	//fancybox
+	$(".fancybox").fancybox({padding: 0, maxWidth: 700, maxHeight: 560, autoSize: true, closeClick: false, openEffect: "elastic", openSpeed: 300, closeEffect: "elastic", closeSpeed: 300});
+	
+	$(".spoiler_button").click(function(){
+	    var parent = $("." + $(this).attr("id"));
+	    if (parent.is(":visible")) {
+	        parent.hide("slow");
+	        $(this).html("развернуть...");
+	    }
+	    else
+	    {
+	        parent.show("slow");
+	        $(this).html("свернуть...");
+	    }
+	})
+
+ 	$('.multiple-items-vert').slick({
+        	infinite: true,
+	        slidesToShow: 5,
+        	slidesToScroll: 1
+	    });
+	
+	    $('.multiple-items-hor').slick({
+        	infinite: true,
+	        slidesToShow: 3,
+	        slidesToScroll: 1
+	    });
+
+	    $('.multiple-items div img, .SingleImage').click(function () {
+	        $.fancybox($(this).clone().css("height", "").css("max-height", "800px"));
+	    });
+
+	    $('.GalleryImage').click(function (e) {
+	        $.fancybox($("<img src='" +$(this).attr("href") + "' style='max-height: 800px;'>"));
+	        return false;
+	    });
+
+	    $(".akcia3").hover(
+        function () {
+            var dropdown = $(this).find('.dropdown');
+            dropdown.css("z-index", 98);
+            dropdown.parent().find(".title").css("z-index", 99);
+            dropdown.stop().show(300);
+        },
+        function () {
+            var dropdown = $(this).find('.dropdown');
+            dropdown.css("z-index", 9);
+            dropdown.parent().find(".title").css("z-index", 10);
+            dropdown.stop().hide(0);
+        }
+);
+
+	    $(".akcia3 .dropdown").each(function () {
+	        $(this).css("width", $(this).parent().find(".title").width() + 52);
+	    });
+});
+
---
+++ Content/themes/ictec/scripts/measurement.js
@@ -0,0 +1,111 @@
+﻿$(document).ready(function () {
+    $(".wpcf7-submit").click(function () {
+
+        isError = false;
+
+        thisButton = $(this);
+
+        clientNameField = thisButton.parent().find("[name=clientName]");
+        clientNameField.removeClass("error");
+        nameChars = " -ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ";
+        nameString = clientNameField.val();
+        if (nameString.length < 3) {
+            clientNameField.addClass("error");
+            isError = true;
+        }
+        else {
+            i = 0;
+            while (ch = nameString.substr(i, 1)) {
+                if (nameChars.indexOf(ch) == -1) {
+                    clientNameField.addClass("error");
+                    isError = true;
+                    break;
+                }
+                i++;
+            }
+        }
+
+        clientPhoneField = thisButton.parent().find("[name=clientPhone]");
+        clientPhoneField.removeClass("error");
+        phoneChars = " +-()1234567890";
+        phoneString = clientPhoneField.val();
+        if (phoneString.length < 5) {
+            clientPhoneField.addClass("error");
+            isError = true;
+        }
+        else {
+            i = 0;
+            while (ch = phoneString.substr(i, 1)) {
+                if (phoneChars.indexOf(ch) == -1) {
+                    clientPhoneField.addClass("error");
+                    isError = true;
+                    break;
+                }
+                i++;
+            }
+        }
+
+        clientEmailField = thisButton.parent().find("[name=clientEmail]");
+        clientEmailField.removeClass("error");
+        emailChars = "_-.@~qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789";
+        emailString = clientEmailField.val();
+        if (emailString.length > 0) {
+            if (emailString.length < 6) {
+                clientEmailField.addClass("error");
+                isError = true;
+            }
+            else {
+                i = 0;
+                HasAt = false;
+                while (ch = emailString.substr(i, 1)) {
+                    if (emailChars.indexOf(ch) == -1) {
+                        clientEmailField.addClass("error");
+                        isError = true;
+                        break;
+                    }
+                    if (ch == "@") {
+                        HasAt = true;
+                    }
+                    i++;
+                }
+                if (!HasAt) {
+                    clientEmailField.addClass("error");
+                    isError = true;
+                }
+            }
+        }
+
+        if (!isError) {
+
+            systemString =  thisButton.parent().find("[name=clientSystem]").val();
+            pageTracker._trackEvent("Заявка на замер - отправка", systemString, phoneString + "|" + nameString, 1);
+            $("#YandexMetrikaWrapper").attr("src", "/MeasurementSend");
+
+            thisButton.attr("disabled", "disabled");
+
+            $.post('/Home/Feedback',
+            {
+                system: systemString,
+                name: nameString,
+                phone: phoneString,
+                email: emailString
+            },
+            function (data) {
+                alert(data.Message);
+                thisButton.removeAttr("disabled");
+                $.fancybox.close();
+            }, "json");
+
+        }
+
+    });
+
+    $(".measurementAsk").click(function () {
+        //dynamicForm = $("#MeasurementFormDynamic");
+        //dynamicForm.find("[name=clientSystem]").val($(this).attr("_system"));
+        //$.fancybox(dynamicForm);
+        //pageTracker._trackEvent("Заявка на замер - открытие", $(this).attr("_system"), document.location.href, 1);
+        //$("#YandexMetrikaWrapper").attr("src", "/MeasurementOpen");
+    })
+
+});
